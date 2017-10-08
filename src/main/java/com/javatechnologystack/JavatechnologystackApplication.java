41f25cc0c764b8c26837bd27ab71703941d7e05d
LaiQingFa <2511695066@qq.com>
2017-10-08 22:12:13 +0800

    java技术栈 first commit

---
+++ src/main/java/com/javatechnologystack/JavatechnologystackApplication.java
@@ -0,0 +1,12 @@
+package com.javatechnologystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatechnologystackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatechnologystackApplication.class, args);
+	}
+}
---
+++ src/main/java/com/javatechnologystack/JvaMain.java
@@ -0,0 +1,11 @@
+package com.javatechnologystack;
+
+public class JvaMain {
+
+
+    public static void main(String [] args){
+
+        System.out.print("nihao*********");
+
+    }
+}
