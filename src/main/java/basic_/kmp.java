5c8dc536d85a58b96fbd1dac3295ce9a6b210aac
LaiQingFa <2511695066@qq.com>
2017-10-21 22:40:47 +0800

    kmp算法

---
+++ src/main/java/basic_/kmp.java
@@ -0,0 +1,16 @@
+package basic_;
+
+public class kmp {
+
+/*
+    kmp算法又称“看毛片”算法，是一个效率非常高的字符串匹配算法。
+
+
+http://blog.csdn.net/yutianzuijin/article/details/11954939/
+
+KMP字符串模式匹配通俗点说就是一种在一个字符串中定位另一个串的高效算法。简单匹配算法的时间复杂度为O(m*n);KMP匹配算法。可以证明它的时间复杂度为O(m+n).。
+http://blog.csdn.net/lin_bei/article/details/1252686
+    */
+
+
+}
