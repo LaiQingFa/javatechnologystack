2561a760d18842b1f547113a82210ab8bca910b8
LaiQingFa <2511695066@qq.com>
2017-10-24 16:43:06 +0800

    创建执行线程的方法2、继承Thread类创建线程类

--- src/main/java/basic_/Make_Thread_Ways.java
+++ src/main/java/basic_/Make_Thread_Ways.java
@@ -8,20 +8,49 @@ public class Make_Thread_Ways {
 }
 
 
+/*方法2、继承Thread类创建线程类
+        （1）定义Thread类的子类，并重写该类的run方法，该run方法的方法体就代表了线程要完成的任务。因此把run()方法称为执行体。
+        （2）创建Thread子类的实例，即创建了线程对象。
+        （3）调用线程对象的start()方法来启动该线程。*/
 
+/*public class FirstThreadTest extends Thread{
+    int i = 0;
+    //重写run方法，run方法的方法体就是现场执行体
+    public void run()
+    {
+        for(;i<100;i++){
+            System.out.println(getName()+"  "+i);
 
+        }
+    }
+    public static void main(String[] args)
+    {
+        for(int i = 0;i< 100;i++)
+        {
+            System.out.println(Thread.currentThread().getName()+"  : "+i);
+            if(i==20)
+            {
+                new FirstThreadTest().start();
+                new FirstThreadTest().start();
+            }
+        }
+    }
 
+}*/
 
 
 /*
  * 一、创建执行线程的方式一：实现 Callable 接口。 相较于实现 Runnable 接口的方式，方法可以有返回值，并且可以抛出异常。
- *
  * 二、执行 Callable 方式，需要 FutureTask 实现类的支持，用于接收运算结果。  FutureTask 是  Future 接口的实现类
  Callable规定的方法是call()，而Runnable规定的方法是run().
  Callable的任务执行后可返回值，而Runnable的任务是不能返回值的。
  call()方法可抛出异常，而run()方法是不能抛出异常的。
- Callable是类似于Runnable的接口，实现Callable接口的类和实现Runnable的类都是可被其它线程执行的任务。
 
+ Callable是类似于Runnable的接口，实现Callable接口的类和实现Runnable的类都是可被其它线程执行的任务。
+（1）创建Callable接口的实现类，并实现call()方法，该call()方法将作为线程执行体，并且有返回值。
+（2）创建Callable实现类的实例，使用FutureTask类来包装Callable对象，该FutureTask对象封装了该Callable对象的call()方法的返回值。
+（3）使用FutureTask对象作为Thread对象的target创建并启动新线程。
+（4）调用FutureTask对象的get()方法来获得子线程执行结束后的返回值
  */
 class TestCallable {
 
