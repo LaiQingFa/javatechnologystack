5b8e17d25a8a466f1728c8c7beced652fd365a42
LaiQingFa <2511695066@qq.com>
2017-10-25 09:54:32 +0800

    String 的hash算法

---
+++ src/main/java/basic_/Hah_function_String.java
@@ -0,0 +1,22 @@
+package basic_;
+
+/**
+ * String 的hash算法
+ */
+public class Hah_function_String {
+
+  /*
+   Hash函数：一种将任意长度的消息压缩到某一固定长度的消息摘要的函数。
+         把任意长度的输入（又叫做预映射， pre-image），通过散列算法，变换成固定长度的输出，该输出就是散列值。
+       MD5 和 SHA1 可以说是目前应用最广泛的Hash算法，而它们都是以 MD4 为基础设计的。
+
+链表查找的时间效率为O(N)，二分法为log2N，B+ Tree为log2N，但Hash链表查找的时间效率为O(1)。
+
+
+http://blog.csdn.net/hengyunabc/article/details/5914934
+
+
+    */
+
+
+}
