6bc36500d63cdd00efc4ef21a0beff4d9ebcfac0
LaiQingFa <2511695066@qq.com>
2017-10-31 17:17:24 +0800

    装饰者模式

---
+++ src/main/java/DesignPattern/Zhuangshizhe.java
@@ -0,0 +1,30 @@
+package DesignPattern;
+/*
+    装饰者模式
+
+    http://www.cnblogs.com/chenxing818/p/4705919.html
+
+    意图： 动态地给一个对象添加一些额外的职责。就增加功能来说， Decorator模式相比生成子类更为灵活。该模式以对客 户端透明的方式扩展对象的功能。
+
+    参与者：
+
+    1.Component（被装饰对象的基类）
+
+      定义一个对象接口，可以给这些对象动态地添加职责。
+
+    2.ConcreteComponent（具体被装饰对象）
+
+      定义一个对象，可以给这个对象添加一些职责。
+
+    3.Decorator（装饰者抽象类）
+
+      维持一个指向Component实例的引用，并定义一个与Component接口一致的接口。
+
+    4.ConcreteDecorator（具体装饰者）
+
+      具体的装饰对象，给内部持有的具体被装饰对象，增加具体的职责。
+
+
+    */
+public class Zhuangshizhe {
+}
