77766f8dcce7e2c06a815ec29d79d7131a0d9a5e
LaiQingFa <2511695066@qq.com>
2017-11-05 22:12:54 +0800

    java内存分配模型（默认HotSpot）

---
+++ src/main/java/MemoryModle/JavaJvm_HotSpot.java
@@ -0,0 +1,9 @@
+package MemoryModle;
+
+public class JavaJvm_HotSpot {
+
+ /*
+    http://www.cnblogs.com/springsource/archive/2013/01/11/2856968.html
+http://ifeve.com/under-the-hood-runtime-data-areas-javas-memory-model/
+    */
+}
