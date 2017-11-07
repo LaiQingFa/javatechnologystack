db1e96d450df715c9c8b0e20064cbb81e447ca3a
LaiQingFa <2511695066@qq.com>
2017-11-07 09:50:11 +0800

    单点登录  sso

--- src/main/java/basic_/Io_.java
+++ src/main/java/basic_/Io_.java
@@ -10,7 +10,7 @@ public class Io_ {
 
     Java网络API允许我们通过套接字（Socket）打开或关闭网络连接，
     但所有的网络通信均是基于Java IO类 InputStream和OutputStream实现的。
-    
+
 java面试基础 ：
     http://ifeve.com/java%E9%9D%A2%E8%AF%95%E9%A2%98-%E5%9F%BA%E7%A1%80%E7%9F%A5%E8%AF%86/
 
