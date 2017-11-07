dbe969110cfd89ea83424d35670d2ef94daf4ea7
LaiQingFa <2511695066@qq.com>
2017-11-07 09:29:20 +0800

    网络io以及java面试基础

---
+++ src/main/java/basic_/Io_.java
@@ -0,0 +1,18 @@
+package basic_;
+
+public class Io_ {
+
+/*
+
+网络io  ：
+
+    http://ifeve.com/java-network/
+
+    Java网络API允许我们通过套接字（Socket）打开或关闭网络连接，
+    但所有的网络通信均是基于Java IO类 InputStream和OutputStream实现的。
+    
+java面试基础 ：
+    http://ifeve.com/java%E9%9D%A2%E8%AF%95%E9%A2%98-%E5%9F%BA%E7%A1%80%E7%9F%A5%E8%AF%86/
+
+    */
+}
