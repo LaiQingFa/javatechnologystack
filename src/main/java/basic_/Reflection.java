41c4f525ea2e54f6ac5a156fca297028c0d137d3
LaiQingFa <2511695066@qq.com>
2017-11-07 09:11:12 +0800

    反射机制

---
+++ src/main/java/basic_/Reflection.java
@@ -0,0 +1,14 @@
+package basic_;
+
+public class Reflection {
+
+/*
+    java反射机制是在运行状态中，对任意一个类，都能够知道这个类的属性和方法；
+ 对任意一个对象，都能都调用它的任意方法和属性。这种动态获取的信息和动态调用对象的方法的功能成为java的反射机制。
+
+http://www.cnblogs.com/bojuetech/p/5896551.html
+一个类在 JVM 中只会有一个Class实例
+
+    */
+
+}
