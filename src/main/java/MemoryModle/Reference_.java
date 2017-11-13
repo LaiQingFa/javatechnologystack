451fa71861357321263c990c422b8979a6db3fb3
LaiQingFa <2511695066@qq.com>
2017-11-13 14:43:39 +0800

    强引用，软引用，弱引用，虚引用。GC

---
+++ src/main/java/MemoryModle/Reference_.java
@@ -0,0 +1,11 @@
+package MemoryModle;
+
+public class Reference_ {
+/*
+Java四种引用包括强引用，软引用，弱引用，虚引用。GC
+    https://www.cnblogs.com/yw-ah/p/5830458.html
+
+http://blog.csdn.net/uniquewonderq/article/details/46408103
+
+    */
+}
