8cf3188b3b00b0758e37e12bdb33ff4df894633f
LaiQingFa <2511695066@qq.com>
2017-11-13 14:46:30 +0800

    指令重排序、内存栅栏

--- src/main/java/MemoryModle/Happens_before.java
+++ src/main/java/MemoryModle/Happens_before.java
@@ -6,7 +6,8 @@ public class Happens_before {
     happens-before俗解
     http://ifeve.com/easy-happens-before/
 
-
+    JVM内存模型-重排序&内存屏障
+    https://www.cnblogs.com/flystar32/p/6684593.html
 
     */
 }
