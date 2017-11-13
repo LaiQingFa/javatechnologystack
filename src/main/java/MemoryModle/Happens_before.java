a729cc1bb1951cd049c4b3ea4a654fa8dcce49bc
LaiQingFa <2511695066@qq.com>
2017-11-13 14:47:36 +0800

    Java 8的内存分代改进

--- src/main/java/MemoryModle/Happens_before.java
+++ src/main/java/MemoryModle/Happens_before.java
@@ -9,5 +9,8 @@ public class Happens_before {
     JVM内存模型-重排序&内存屏障
     https://www.cnblogs.com/flystar32/p/6684593.html
 
+    Java 8的内存分代改进
+    http://blog.csdn.net/chlu113/article/details/51890469
+
     */
 }
