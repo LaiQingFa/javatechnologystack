ed1fb66988d5d58bc5b416af71f3529ebfbd1b28
LaiQingFa <2511695066@qq.com>
2017-11-15 09:13:59 +0800

    独占锁、共享锁；可重入的独占锁ReentrantLock、共享锁 实现原理     公平锁和非公平锁

---
+++ src/main/java/lockAndconcurrent_container/Lock.java
@@ -0,0 +1,22 @@
+package lockAndconcurrent_container;
+
+public class Lock {
+
+/*
+    http://blog.csdn.net/wojiushiwo945you/article/details/42292999
+    独占锁、共享锁；可重入的独占锁ReentrantLock、共享锁 实现原理
+    公平锁和非公平锁
+    java并发包提供的加锁模式分为独占锁和共享锁，独占锁模式下，
+    每次只能有一个线程能持有锁，ReentrantLock就是以独占方式实现的互斥锁。
+    共享锁，则允许多个线程同时获取锁，并发访问 共享资源，如：ReadWriteLock。
+
+    锁的公平与非公平，是指线程请求获取锁的过程中，是否允许插队。在公平锁上，
+    线程将按他们发出请求的顺序来获得锁而非公平锁则允许在线程发出请求后立即尝试获取锁，
+    如果可用则可直接获取锁，尝试失败才进行排队等待
+
+
+
+
+    */
+
+}
