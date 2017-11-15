183eec73025bb951f0475bd38483d1ba5106b93e
LaiQingFa <2511695066@qq.com>
2017-11-15 09:38:26 +0800

    ConcurrentLinkedQueue的实现原理

--- src/main/java/lockAndconcurrent_container/Collection_all.java
+++ src/main/java/lockAndconcurrent_container/Collection_all.java
@@ -15,6 +15,13 @@ public class Collection_all {
     https://my.oschina.net/xianggao/blog/393990?fromerr=OPResiVf
 
 
+    ConcurrentLinkedQueue的实现原理
+    http://ifeve.com/concurrentlinkedqueue/
+    ConcurrentLinkedQueue是一个基于链接节点的无界线程安全队列，
+    它采用先进先出的规则对节点进行排序，当我们添加一个元素的时候，
+    它会添加到队列的尾部，当我们获取一个元素时，它会返回队列头部的元素。它采用了“wait－free”算法来实现
+
+
     */
 
 }
