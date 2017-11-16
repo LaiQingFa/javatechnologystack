b487ec9746bfaf8811fe6742f8202587f213e2eb
LaiQingFa <2511695066@qq.com>
2017-11-16 17:33:26 +0800

    servlet开发存在的问题：映射问题、参数获取问题、格式化转换问题、返回值处理问题、视图渲染问题

---
+++ src/main/java/SpringMVCFrame/Servlet_.java
@@ -0,0 +1,17 @@
+package SpringMVCFrame;
+
+public class Servlet_ {
+
+/*
+    映射问题：https://www.cnblogs.com/warrior4236/p/5975496.html
+
+    参数获取问题：https://www.cnblogs.com/solverpeng/p/5733310.html
+
+    格式化转换问题：http://blog.csdn.net/yangsen251024/article/details/4790737
+
+
+    返回值处理问题：http://www.linuxidc.com/Linux/2016-08/133973.htm
+
+    视图渲染（包含视图解析）问题：https://www.cnblogs.com/solverpeng/p/5743609.html
+    */
+}
