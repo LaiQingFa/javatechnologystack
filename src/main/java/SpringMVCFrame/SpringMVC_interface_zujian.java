d53f1d38e52b3372250094259eb97b4aff8ca5c9
LaiQingFa <2511695066@qq.com>
2017-11-19 16:29:22 +0800

    SpringMVC为解决上述问题开发的几大组件及接口：HandlerMapping、HandlerAdapter、  HandlerMethodArgumentResolver、HttpMessageConverter、Converter、GenericConverter、  HandlerMethodReturnValueHandler、ViewResolver、MultipartResolver

---
+++ src/main/java/SpringMVCFrame/SpringMVC_interface_zujian.java
@@ -0,0 +1,20 @@
+package SpringMVCFrame;
+
+public class SpringMVC_interface_zujian {
+
+ /*
+
+
+ SpringMVC为解决上述问题开发的几大组件及接口：HandlerMapping、HandlerAdapter、
+ HandlerMethodArgumentResolver、HttpMessageConverter、Converter、GenericConverter、
+ HandlerMethodReturnValueHandler、ViewResolver、MultipartResolver
+
+
+http://blog.csdn.net/hustwht/article/details/52280977
+
+
+
+
+    */
+
+}
