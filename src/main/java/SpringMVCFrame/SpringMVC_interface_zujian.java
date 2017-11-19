2626d3837a06f24c33759cc0c232822602f3b4f6
LaiQingFa <2511695066@qq.com>
2017-11-19 16:31:07 +0800

    DispatcherServlet、容器、组件三者之间的关系

--- src/main/java/SpringMVCFrame/SpringMVC_interface_zujian.java
+++ src/main/java/SpringMVCFrame/SpringMVC_interface_zujian.java
@@ -15,6 +15,12 @@ http://blog.csdn.net/hustwht/article/details/52280977
 
 
 
+DispatcherServlet - 容器 —— DispatcherServlet对容器进行初始化
+容器 - 组件 —— 容器对组件进行全局管理
+DispatcherServlet - 组件 —— DispatcherServlet对组件进行逻辑调用
+
+http://blog.csdn.net/h396071018/article/details/8268488
+
     */
 
 }
