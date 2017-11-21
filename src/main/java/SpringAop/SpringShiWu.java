4cb8a3660053a499f111c1d4172bda27bf66a26a
LaiQingFa <2511695066@qq.com>
2017-11-21 16:14:08 +0800

    针对各种各样的事务，Spring如何定义事务体系的接口，以及如何融合jdbc事务和Hibernate事务的

--- src/main/java/SpringAop/SpringShiWu.java
+++ src/main/java/SpringAop/SpringShiWu.java
@@ -7,5 +7,10 @@ public class SpringShiWu {
 
     Hibernate对事务的改进
     https://www.cnblogs.com/linkworld/p/7697697.html
+
+    针对各种各样的事务，Spring如何定义事务体系的接口，以及如何融合jdbc事务和Hibernate事务的
+    http://www.mamicode.com/info-detail-1248286.html
+
+
     */
 }
