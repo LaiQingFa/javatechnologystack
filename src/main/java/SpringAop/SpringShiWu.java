10fee72c07e55fe9fd9f098d92e12f9d1b5b8319
LaiQingFa <2511695066@qq.com>
2017-11-21 16:12:36 +0800

    Hibernate对事务的改进

--- src/main/java/SpringAop/SpringShiWu.java
+++ src/main/java/SpringAop/SpringShiWu.java
@@ -5,5 +5,7 @@ public class SpringShiWu {
     jdbc事务存在的问题
     http://blog.csdn.net/dzy21/article/details/51941419
 
+    Hibernate对事务的改进
+    https://www.cnblogs.com/linkworld/p/7697697.html
     */
 }
