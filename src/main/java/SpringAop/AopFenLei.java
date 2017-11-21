8186f4f2f189bb4069f94d3eedf213b00309ac4c
LaiQingFa <2511695066@qq.com>
2017-11-21 16:00:52 +0800

    AOP联盟定义的概念或接口：Pointcut（概念，没有定义对应的接口）、Joinpoint、Advice、MethodInterceptor、MethodInvocation

--- src/main/java/SpringAop/AopFenLei.java
+++ src/main/java/SpringAop/AopFenLei.java
@@ -9,5 +9,7 @@ public class AopFenLei {
 AOP联盟、aspectj、jboss AOP、Spring自身实现的AOP、Spring嵌入aspectj。
 http://www.codeweblog.com/spring-aop%E6%BA%90%E7%A0%81%E5%88%86%E6%9E%90-%E4%B8%80-aop%E4%BB%8B%E7%BB%8D%E5%92%8Caspectj-springaop%E5%85%A5%E9%97%A8/
 
+AOP联盟定义的概念或接口：Pointcut（概念，没有定义对应的接口）、Joinpoint、Advice、MethodInterceptor、MethodInvocation
+https://yq.aliyun.com/articles/38884
     */
 }
