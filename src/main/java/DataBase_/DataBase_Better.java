1df61b654c57f39877975830bf2a7868cb46cc98
LaiQingFa <2511695066@qq.com>
2017-11-23 10:23:36 +0800

    insert into select语句的加锁情况

--- src/main/java/DataBase_/DataBase_Better.java
+++ src/main/java/DataBase_/DataBase_Better.java
@@ -11,7 +11,8 @@ Mysql数据库调优和性能优化
 深入理解mysql的Record Locks、Gap Locks、Next-Key Locks
 http://blog.csdn.net/thekenofdis/article/details/54288894
 
-
+insert into select语句的加锁情况
+http://blog.csdn.net/woshizhangliang999/article/details/53693967
 
 
 
