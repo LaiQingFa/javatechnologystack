cf5acbb7a8fa4d9a8b6e2d5f33768570fa519e29
LaiQingFa <2511695066@qq.com>
2017-11-25 10:32:26 +0800

    undo redo 原理

---
+++ src/main/java/DataBase_/UndoRedoBinlog__.java
@@ -0,0 +1,22 @@
+package DataBase_;
+
+public class UndoRedoBinlog__ {
+
+
+/*
+
+undo redo 都可以实现持久化，他们的流程是什么？为什么选用redo来做持久化
+
+http://blog.csdn.net/alexdamiao/article/details/51872477
+
+
+
+
+
+
+
+
+
+
+    */
+}
