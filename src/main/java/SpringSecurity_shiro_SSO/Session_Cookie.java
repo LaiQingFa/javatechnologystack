9fae687574622b03d4a5b8dfb4d5713817d446e7
LaiQingFa <2511695066@qq.com>
2017-11-25 10:59:13 +0800

    CAS实现SSO

--- src/main/java/SpringSecurity_shiro_SSO/Session_Cookie.java
+++ src/main/java/SpringSecurity_shiro_SSO/Session_Cookie.java
@@ -9,6 +9,8 @@ http://blog.csdn.net/dreamseeker_1314/article/details/51099160
 SpringSecurity的认证过程以及与Session的关系
 http://blog.csdn.net/zhangchen2449/article/details/52623122
 
+CAS实现SSO
+http://blog.csdn.net/javaloveiphone/article/details/52439613
 
     */
 
