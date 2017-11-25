f1f03917a6da407fd59e28afa800789b5b06943d
LaiQingFa <2511695066@qq.com>
2017-11-25 10:57:27 +0800

    Session和Cookie的区别和联系以及Session的实现原理

---
+++ src/main/java/SpringSecurity_shiro_SSO/Session_Cookie.java
@@ -0,0 +1,13 @@
+package SpringSecurity_shiro_SSO;
+
+public class Session_Cookie {
+
+   /*
+    Session和Cookie的区别和联系以及Session的实现原理
+http://blog.csdn.net/dreamseeker_1314/article/details/51099160
+
+
+
+    */
+
+}
