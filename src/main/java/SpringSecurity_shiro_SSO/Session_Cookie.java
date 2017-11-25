c87832ae6d70003aac0e86323b8fd5a4fc1113dd
LaiQingFa <2511695066@qq.com>
2017-11-25 10:58:15 +0800

    SpringSecurity的认证过程以及与Session的关系

--- src/main/java/SpringSecurity_shiro_SSO/Session_Cookie.java
+++ src/main/java/SpringSecurity_shiro_SSO/Session_Cookie.java
@@ -6,6 +6,8 @@ public class Session_Cookie {
     Session和Cookie的区别和联系以及Session的实现原理
 http://blog.csdn.net/dreamseeker_1314/article/details/51099160
 
+SpringSecurity的认证过程以及与Session的关系
+http://blog.csdn.net/zhangchen2449/article/details/52623122
 
 
     */
